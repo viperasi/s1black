9434c799a5173e712056771cc44be8c2815ba54a
viperasi <viperasi@gmail.com>
2016-12-26 15:51:40 +0800

    回帖屏蔽

--- content.js
+++ content.js
@@ -3,9 +3,11 @@ $(function () {
     chrome.runtime.onMessage.addListener(function (request, sender, sendRequest) {
         if (request.type === 'remove') {
             blacklist();
+            blackthread();
         }
     });
     blacklist();
+    blackthread();
 })
 
 
@@ -24,6 +26,37 @@ function blacklist() {
             $.each(list, function (i, name) {
                 if (tname === name) {
                     $(thread).hide();
+                }
+            })
+        });
+    });
+}
+
+function blackthread(){
+    chrome.storage.local.get(["s1blacklist"], function (item) {
+        var list;
+        if (!item.s1blacklist) {
+            list = [];
+        } else {
+            list = item.s1blacklist;
+        }
+        var tlist = $("a.xw1");
+        $.each(tlist, function (index, thread) {
+            var tname = $(thread).text();
+            $.each(list, function (i, name) {
+                if (tname === name) {
+                    var div_parent = $(thread).parents('div[id^="post_"]');
+                    div_parent.children().hide();
+                    var span = $("<span>").css({
+                        "display":"block",
+                        "height": "20px",
+                        "text-align": "center",
+                        "border": "solid 1px"
+                    }).append("黑名单已隐藏, 这是你不想看到的人的言论, 你就点下去吧.").click(function(){
+                        span.hide();
+                        div_parent.children("table").show();
+                    });
+                    div_parent.append(span);
                 }
             })
         });
